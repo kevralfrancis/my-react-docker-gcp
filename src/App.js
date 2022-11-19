b40be96d123ef05b8755a3b13b2f0935aa24ac5c
Kevral <kevralfrancis29@gmail.com>
2022-11-19 18:45:38 -0500

    cicd commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Kevral successful pipeline deployment
         </p>
         <a
           className="App-link"
